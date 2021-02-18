c84727b2996ace19ac8e7e0b491aa73c4780372d
Daniel Waruo <waruodaniel@gmail.com>
2021-02-18 12:02:58 +0300

    enabled pip install

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setuptools.setup(
     description="Python wrapper for the IPAY api",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/pypa/sampleproject",
+    url="https://github.com/daniel-waruo/ipay-wrapper",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
